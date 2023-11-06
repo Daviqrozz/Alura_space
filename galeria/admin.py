9d58371f79047a5d7637d538dc2cf72fafb343a7
Daviqueiroz <daviqrozz2@gmail.com>
2023-11-06 14:29:19 -0300

    'update'

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+class ListarFotos(admin.ModelAdmin):
+    list_display = ('id','nome','legenda','categoria','publicada')
+    list_display_links = ('id','nome')
+    search_fields = ['nome']
+    list_editable = ['publicada']
+    list_filter = ['categoria']
+
+admin.site.register(Fotografia,ListarFotos)
--- galeria/views.py
+++ galeria/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from galeria.models import Fotografia
 
-
 def index(request):
-    fotografias = Fotografia.objects.all()
-    return render(request, 'galeria/index.html',{"dados":fotografias})
+    fotografia = Fotografia.objects.order_by('data_fotografia').filter(publicada=True)
+    return render(request, 'galeria/index.html',{"dados":fotografia})
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html',)
+def imagem(request,foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html',{"fotografia":fotografia})
+
+def buscar(request):
+    fotografia = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
+
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar'].upper()
+        if nome_a_buscar: 
+            fotografia = fotografia.filter(nome__icontains=nome_a_buscar)
+    return render (request, "galeria/buscar.html", {"dados": fotografia})
